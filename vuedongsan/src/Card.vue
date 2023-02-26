2ec32b050bc2f2bda7a670e8a9f92d2b9a326c13
jje0ngyu <109578927+jje0ngyu@users.noreply.github.com>
2023-02-27 02:10:53 +0900

    v-model 학습

--- vuedongsan/src/Card.vue
+++ vuedongsan/src/Card.vue
@@ -1,17 +1,31 @@
 <template>
     <div>
     <img :src="원룸들.image" class="room-img">
-    <h4>{{원룸들.title}}</h4>
+    <h4 @click="함수">{{원룸들.title}}</h4>
     <p>{{원룸들.price}}원</p>
   </div>
+  <!-- custom event
+   이걸로 부모데이터 수정해야 된다. (실은 데이터 수정해달라는 메시지이고, 실제로 데이터를 수정하지는 않는다.)
+    $emit('작명', 데이터)
+    $ 표시는 뷰에 내장된 것
+    자식이 보낸 데이터는 부모 컴포넌트에서 $event 변수를 사용하면 담긴 내용을 확인할 수 있다.
+  -->
 </template>
 
 <script>
 export default {
     name : 'Card',
     props: {
-      원룸들 : Object,
-      누른거 : Number,
+        // props로 보낸 데이터는 수정금지 (Read-only)
+        // 부모의 데이터를 수정 (custom event)
+        원룸들 : Object,
+    },
+    // $emit()을 함수 안에서 사용하고 싶으면
+    // + props든, data든, $변수든 밑에서 쓸 땐 this 붙여야 한다.
+    methods : {
+        함수(){
+            this.$emit('openModal', this.원룸들.id)
+        }
     }
     
 }
--- vuedongsan/src/Modal.vue
+++ vuedongsan/src/Modal.vue
@@ -5,8 +5,23 @@
       <h4>{{ 원룸들[누른거].title }}</h4>
       <img :src="원룸들[누른거].image" style="width:100%">
       <p>{{원룸들[누른거].content}}</p>
-      <p>{{원룸들[누른거].price}}</p>
-      <!-- <button @click="모달창열렸니 = false">닫기</button> -->
+      <!-- <input @input="month = $event.target.value"> -->
+      <input v-model.number="month">
+      <!-- v-model="데이터이름"을 사용하면 input류에 입력한 데이터를 "데이터이름"에 바로 값을 넣어준다.
+        <textarea> 에서도 v-model을 사용할 수 있다. 단,
+            문자로 입력받기 때문에 data()에서 초깃값을 스트링 타입으로 해줘야한다.
+            ex) month = '',
+        <select> 에서도 v-model을 사용할 수 있다.
+            <option>의 값을 "데이터이름"에 저장된다.
+        <input type="checkbox" v-model="데이터이름">
+            checkbox의 값은 true/false로 표시
+      -->
+      <!--
+        @input : 입력할 때마다 실행하기
+        @change : 입력 후 다른 곳을 마우스클릭 했을 때 실행하기
+      -->
+      <p>{{month}}개월 선택함 : {{원룸들[누른거].price * month}}원</p>
+      <button @click="함수">닫기</button>
       <!-- props는 read-only 이므로, 받아온 거 수정하면 큰일남
        잘못된 예시 : <button @click="모달창열렸니 = false">닫기</button>
       -->
@@ -17,12 +32,27 @@
 <script>
 export default {
     name : 'Modal',
+    data(){
+        return {
+            month : 1, // 초기값
+        }
+    },
+
+    // 데이터를 감시하려면? watch!
+    watch : {
+
+    },
     props: {
         // 자식은 props로 받은 거 등록
         // props : {데이터이름 : 자료형이름}
         원룸들 : Array, // 받아온 데이터의 자료형 이름을 대문자로
         누른거 : Number,
         모달창열렸니 : Boolean,
+    },
+    methods: {
+        함수(){
+            this.$emit('closeModal')
+        }
     }
 }
 </script>
